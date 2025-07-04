abe4d18863adcad1b2de2d7cb3b776b1e49deb76
abhishek krishna <166230352+xGravityXD@users.noreply.github.com>
2025-07-05 01:00:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+
+  toggleBtn.textContent = document.body.classList.contains("dark-theme")
+    ? "☀️ Light Mode"
+    : "🌙 Dark Mode";
+});
