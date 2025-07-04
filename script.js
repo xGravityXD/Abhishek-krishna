1f5f7f918e7b52e0dfbc0ba46a03dbafb8c19dde
abhishek krishna <166230352+xGravityXD@users.noreply.github.com>
2025-07-05 01:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const toggleBtn = document.getElementById("theme-toggle");
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
+const toggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
 
-  toggleBtn.textContent = document.body.classList.contains("dark-theme")
-    ? "☀️ Light Mode"
-    : "🌙 Dark Mode";
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  body.classList.add('dark-theme');
+  toggleBtn.textContent = '🌞 Light Mode';
+}
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  if (body.classList.contains('dark-theme')) {
+    toggleBtn.textContent = '🌞 Light Mode';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    toggleBtn.textContent = '🌙 Dark Mode';
+    localStorage.setItem('theme', 'light');
+  }
 });
